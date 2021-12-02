3c92e426c721fa17b7bc43ce0ca619d8812e15ac
gddcx <1021312512@qq.com>
2021-12-02 14:20:22 +0800

    修改输出形状

--- network.py
+++ network.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torchvision
 
@@ -43,7 +44,7 @@ class Backbone(nn.Module):
         )
 
     def forward(self, data):
-        out1 = self.subnet1(data)
+        out1 = self.subnet1(data) # TODO: Conv4-3要L2处理尺度问题
         out2 = self.subnet2(out1)
         out3 = self.subnet3(out2)
         out4 = self.subnet4(out3)
@@ -52,49 +53,35 @@ class Backbone(nn.Module):
         return out1, out2, out3, out4, out5, out6
 
 
-class ClassifierHead(nn.Module):
+class Detection(nn.Module):
     def __init__(self, num_category):
         super().__init__()
         # channels 512, 1024, 512, 256, 256, 256
+        self.num_category = num_category
         self.net = nn.ModuleList()
-        self.net.append(nn.Conv2d(in_channels=512, out_channels=4 * num_category, kernel_size=3, stride=1, padding=1))
-        self.net.append(nn.Conv2d(in_channels=1024, out_channels=6 * num_category, kernel_size=3, stride=1, padding=1))
-        self.net.append(nn.Conv2d(in_channels=512, out_channels=6 * num_category, kernel_size=3, stride=1, padding=1))
-        self.net.append(nn.Conv2d(in_channels=256, out_channels=6 * num_category, kernel_size=3, stride=1, padding=1))
-        self.net.append(nn.Conv2d(in_channels=256, out_channels=4 * num_category, kernel_size=3, stride=1, padding=1))
-        self.net.append(nn.Conv2d(in_channels=256, out_channels=4 * num_category, kernel_size=3, stride=1, padding=1))
+        self.net.append(nn.Conv2d(in_channels=512, out_channels=4 * 4 + 4 * num_category, kernel_size=3, stride=1, padding=1))
+        self.net.append(nn.Conv2d(in_channels=1024, out_channels=6 * 4 + 6 * num_category, kernel_size=3, stride=1, padding=1))
+        self.net.append(nn.Conv2d(in_channels=512, out_channels=6 * 4 + 6 * num_category, kernel_size=3, stride=1, padding=1))
+        self.net.append(nn.Conv2d(in_channels=256, out_channels=6 * 4 + 6 * num_category, kernel_size=3, stride=1, padding=1))
+        self.net.append(nn.Conv2d(in_channels=256, out_channels=4 * 4 + 4 * num_category, kernel_size=3, stride=1, padding=1))
+        self.net.append(nn.Conv2d(in_channels=256, out_channels=4 * 4 + 4 * num_category, kernel_size=3, stride=1, padding=1))
 
     def forward(self, data):
-        out = []
+        out_list = []
+        batch_size = data[0].shape[0]
         for idx in range(len(self.net)):
-            out.append(self.net[idx](data[idx]))
-        return out
-
-
-class LocationHead(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.net = nn.ModuleList()
-        self.net.append(nn.Conv2d(in_channels=512, out_channels=4 * 4, kernel_size=3, stride=1, padding=1))
-        self.net.append(nn.Conv2d(in_channels=1024, out_channels=6 * 4, kernel_size=3, stride=1, padding=1))
-        self.net.append(nn.Conv2d(in_channels=512, out_channels=6 * 4, kernel_size=3, stride=1, padding=1))
-        self.net.append(nn.Conv2d(in_channels=256, out_channels=6 * 4, kernel_size=3, stride=1, padding=1))
-        self.net.append(nn.Conv2d(in_channels=256, out_channels=4 * 4, kernel_size=3, stride=1, padding=1))
-        self.net.append(nn.Conv2d(in_channels=256, out_channels=4 * 4, kernel_size=3, stride=1, padding=1))
-
-    def forward(self, data):
-        out = []
-        for idx in range(len(self.net)):
-            out.append(self.net[idx](data[idx]))
-        return out
+            out = self.net[idx](data[idx])
+            out = out.permute(0, 2, 3, 1) # bs, fms, fms, dbn*(4+num_category)
+            out = out.reshape(batch_size, -1, 4+self.num_category)
+            out_list.append(out)
+        return torch.cat(out_list, dim=1)
 
 
 class SSDNet(nn.Module):
     def __init__(self):
         super().__init__()
         self.backbone = Backbone(pretrain=True)
-        self.classifier = ClassifierHead(num_category=20)
-        self.location = LocationHead()
+        self.detection = Detection(num_category=21)
         self._initialize_weights()
 
     def _initialize_weights(self):
@@ -102,16 +89,10 @@ class SSDNet(nn.Module):
             if isinstance(m, nn.Conv2d):
                 # nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity='relu')
                 nn.init.xavier_normal_(m.weight)
-                if m.bias:
+                if m.bias is not None:
                     nn.init.constant_(m.bias, 0)
 
     def forward(self, data):
         out = self.backbone(data)
-        out_class = self.classifier(out)
-        out_loc = self.location(out)
-        return out_class, out_loc
-
-
-if __name__ == "__main__":
-    import torch
-    print(SSDNet()(torch.randn(1, 3, 300, 300)).shape)
+        out = self.detection(out)
+        return out
